69f323ad82c6c275581a65d881047a4d6c92b10c
Emmanuel Tom Jose <84658038+leantos@users.noreply.github.com>
2024-12-03 15:08:17 +0530

    removed animations too tacky

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,20 +3,19 @@ import React from "react";
 export function Main() {
   const [ingredients, setIngredients] = React.useState([]);
 
-  const list = ingredients.map((ingredients) => (
-    <li key={ingredients}>{ingredients}</li>
-  ));
-
-  function onsubmitted(event) {
-    event.preventDefault();
+  function addIngredient(event) {
+    event.preventDefault()
     const formData = new FormData(event.currentTarget);
     const newIngredient = formData.get("ingredient");
     setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
   }
+  const list = ingredients.map((ingredients) => (
+    <li key={ingredients}>{ingredients}</li>
+  ));
 
   return (
     <main>
-      <form onSubmit={onsubmitted} className="add-ingredient">
+      <form onSubmit={addIngredient} className="add-ingredient">
         <input
           type="text"
           aria-label="Add Ingredient"
