1fae5a95a2489523552d29f25103afb7f903c316
Emmanuel Tom Jose <84658038+leantos@users.noreply.github.com>
2024-12-03 10:24:31 +0530

    small

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-const logo='/src/assets/chef.svg';
-export function Header(){
-    return(
-        <div className="header">
-            <img src={logo} alt="Chef Logo" className="chef-logo"/>
-            <h1 className="site-title">Chef Claude</h1>
-        </div>
-    )
+const logo = "/src/assets/chef.svg";
+export function Header() {
+  return (
+    <div className="header">
+      <img src={logo} alt="Chef Logo" className="chef-logo" />
+      <h1 className="site-title">Chef Claude</h1>
+    </div>
+  );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,32 @@
-export function Main(){
-    return(
-        <main>
-            <form className="add-ingredient">
-                <input type="text" aria-label="Add Ingredient" placeholder="e.g. oregano"/>
-                <button>Add Ingredient</button>
-            </form>
-        </main>
-    );
+export function Main() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  function onsubmitted(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    ingredients.push(newIngredient);
+
+    alert(ingredients);
+  }
+
+  const list = ingredients.map((ingredients) => (
+    <li key={ingredients}>{ingredients}</li>
+  ));
+
+  return (
+    <main>
+      <form onSubmit={onsubmitted} className="add-ingredient">
+        <input
+          type="text"
+          aria-label="Add Ingredient"
+          placeholder="e.g. oregano"
+          name="ingredient"
+        />
+        <button>Add Ingredient</button>
+      </form>
+      <ul>{list}</ul>
+    </main>
+  );
 }
